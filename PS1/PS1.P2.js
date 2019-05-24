ffbf1f0e85a9a591575196af1e0cb2c7ebf909a4
Jing <xjing@bu.edu>
2019-05-24 00:22:30 -0400

    ps1pr2 completed

---
+++ PS1/PS1.P2.js
@@ -0,0 +1,17 @@
+
+function evaluate(str){
+    const a = eval (str);
+    return a
+}
+const expression1 = '4+2';
+console.log(evaluate(expression1));
+
+const expression2 = '5*7';
+console.log(evaluate(expression2));
+
+const expression3 = '6-1';
+console.log(evaluate(expression3));
+
+const expression4 = '9/2';
+console.log(evaluate(expression4));
+

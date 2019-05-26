d6573b51650dd62acff8d7cec1cba63deb363f44
Jing <xjing@bu.edu>
2019-05-26 18:58:14 -0400

    ps1pr3 complete

--- PS1/PS1.P3.js
+++ PS1/PS1.P3.js
@@ -1,1 +1,19 @@
-const spp = (str,cb) => cb()
+const transfer = (str, func) => func(str);
+
+const inputString = 'supercalifragilisticexpialidocious';
+
+let broke = str => str.match(/(c[^c]*|[^c]*)/g);
+let replaceA = str => {
+    let ret = {};
+    let count = 0;
+    ret['originalString'] = str;
+    ret['modifiedString'] = str.replace(/a/g, (sub,index) => {count++;return 'A'});
+    ret['numberReplaced'] = count;
+    ret['length']= str.length;
+    return ret
+};
+
+console.log(`${transfer(inputString, broke)}`);
+console.log(`${JSON.stringify(transfer(inputString, replaceA))}`);
+
+(str,cb) => cb()

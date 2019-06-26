bb84759d1ac13835886e104adb262a4c395a9ad9
Jing <xjing@bu.edu>
2019-06-25 23:09:59 -0400

    add config

---
+++ PS6/src/app/config/config.ts
@@ -0,0 +1,3 @@
+export const endpoints = {
+  YelpEndpoint: 'http://localhost:3000/ps4/boston/yelp/business?key='
+};
--- PS6/src/app/services/yelp.service.ts
+++ PS6/src/app/services/yelp.service.ts
@@ -2,12 +2,13 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {ShopModel} from '../models/shopModel';
+import {endpoints} from '../config/config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class YelpService {
-  localYelpEndpoint = 'http://localhost:3000/ps4/boston/yelp/business?key=';
+  localYelpEndpoint = endpoints.YelpEndpoint;
 
   getShops(term: string): Observable<ShopModel[]> {
     return this.httpClient.get<ShopModel[]>(this.localYelpEndpoint + term);
